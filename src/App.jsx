e9f8bacbf734c6682e0dbda74d181cc74a3c1cb2
HoussemDAAS <houssemdaas2@gmail.com>
2024-01-16 12:42:14 +0100

    useState exemple

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import Header from "./assets/Components/Header/Header";
 import { CORE_CONCEPTS } from "./data";
 import CoreConcepts from "./assets/Components/CoreConcepts/CoreConcepts";
 import TabButton from "./assets/Components/TabButton/TabButton";
-
+import { useState } from "react";
 
 function App() {
+ const [TabContent, setTabContent] = useState('Pls click on tab button');
+
   function handleSelect(Selected) {
-    console.log(Selected);
+    setTabContent(Selected);
+    console.log(TabContent);
    }
   return (
     <div>
@@ -30,7 +33,7 @@ function App() {
           <TabButton onSelect={() =>handleSelect("Pops")}>Pops</TabButton>
           <TabButton onSelect={() =>handleSelect("STATE")}>State</TabButton>
         </menu>
-        
+        {TabContent}
        </section>
       </main>
     </div>
